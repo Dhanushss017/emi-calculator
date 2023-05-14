9f5e8c0ac6ee17a904532e68bc28321677f243fe
Dhanushss017 <117816081+Dhanushss017@users.noreply.github.com>
2023-05-14 19:59:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-
 function computeLoan() {
-    const calculete_btn = document.querySelector("#btn-loan");
-    const amount = document.querySelector("#amount").value;
-    const interest_rate = document.querySelector("#interest_rate").value;
-    const months = document.querySelector("#months").value;
-    const interest = (amount * (interest_rate * 0.01)) / months;
-    let payment = (amount / months + interest).toFixed(2);
-    payment = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    document.querySelector(
-        "#payment"
-    ).innerHTML = `Monthly Payment = ₹${payment}`;
+  const amount = parseFloat(document.getElementById("amount").value);
+  const interest_rate = parseFloat(document.getElementById("interest_rate").value);
+  const months = parseFloat(document.getElementById("months").value);
+  const interest = (amount * (interest_rate * 0.01)) / months;
+  const payment = (amount / months + interest).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  document.getElementById("payment").innerHTML = `Monthly Payment = ₹${payment}`;
 }
